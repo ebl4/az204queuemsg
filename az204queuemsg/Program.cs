c8b4e69592c270603cb1e75677fa8dca279a4575
Edson <edbdelima@gmail.com>
2023-03-24 20:36:10 -0300

    Adicionar arquivos de projeto.

---
+++ az204queuemsg/Program.cs
@@ -0,0 +1,19 @@
+﻿// Get the connection string from app settings
+using Azure.Storage.Queues;
+using System.Configuration;
+
+string connectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
+string queueName = "QUEUE_NAME";
+string mensage = "Teste Mensage";
+
+// Instantiate a QueueClient which will be used to create and manipulate the queue
+QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+// Create the queue
+queueClient.CreateIfNotExists();
+
+if (queueClient.Exists())
+{
+    // Send a mensage to the queue
+    queueClient.SendMessage(mensage);
+}
